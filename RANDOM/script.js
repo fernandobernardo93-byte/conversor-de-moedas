8475b38c56e774b3f1383afd6fa688c22bebcfd2
fernandobernardo93-byte <fernando.bernardo93@gmail.com>
2026-07-16 10:26:06 -0300

    Delete RANDOM directory

--- RANDOM/script.js
+++
@@ -1,8 +0,0 @@
-function sortear(){
-   
-    const valorMin = Math.ceil(document.querySelector('#input-min').value)
-    const valorMax = Math.floor(document.querySelector('#input-max').value)
-
-    const valorSorteado = Math.floor(Math.random() * (valorMax - valorMin + 1) + valorMin);
-    alert(valorSorteado);
-}
